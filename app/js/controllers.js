e46e395062a6d73e2a5d3a11e1524cd6e26b8ff8
Monica Chew <mchew@mchew-22096.local>
2014-10-13 17:00:52 -0700

    cleanup

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,9 +4,28 @@
 
 var phonecatApp = angular.module('phonecatApp', []);
 
+var issuerCount = {};
 phonecatApp.controller('PhoneListCtrl', function($scope, $http) {
   $http.get('data/cas.json').success(function(data) {
     $scope.certs = data;
+    var log = "";
+    var issuers = [];
+    var x;
+    for (var i in data) {
+      var cert = data[i];
+      if (!issuerCount[cert.issuer]) {
+        issuerCount[cert.issuer] = 0;
+      }
+      issuerCount[cert.issuer] = issuerCount[cert.issuer] + 1;
+    }
+    for (var j in issuerCount) {
+      var foo = {};
+      foo.name = j;
+      foo.count = issuerCount[j];
+      issuers.push(foo);
+    }
+    $scope.topIssuers = issuers;
+    $scope.log = JSON.stringify(issuerCount);
   });
 
   $scope.orderProp = 'notBefore';
